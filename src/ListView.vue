a3e6ca85d15ea0bfd1fefbb5664ce2d1eff87199
Hui Yiqun <huiyiqun@gmail.com>
2017-02-16 16:23:20 +0800

    silly tabs

--- src/ListView.vue
+++ src/ListView.vue
@@ -1,5 +1,9 @@
 <template>
-  <p>List</p>
+  <ul class="nav nav-tabs">
+    <li role="presentation" class="active"><a href="#/list/">Home</a></li>
+    <li role="presentation"><a href="#">Profile</a></li>
+    <li role="presentation"><a href="#">Messages</a></li>
+  </ul>
 </template>
 
 <script>
