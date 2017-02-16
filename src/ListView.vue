effb6831200444ade592afcb52226f8187083837
Hui Yiqun <huiyiqun@gmail.com>
2017-02-16 22:50:10 +0800

    tabs for categories

--- src/ListView.vue
+++ src/ListView.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <ul class="nav nav-tabs">
-      <li role="presentation" class="active"><router-link :to="{ name: 'list', params: { category: 'home'}}">Home</router-link></li>
-      <li role="presentation"><a href="#">Profile</a></li>
-      <li role="presentation"><a href="#">Messages</a></li>
+      <li v-for="c in channels['Categories']" role="presentation" :class="{active: c['Name'] == category}">
+        <router-link :to="link(c)">{{c['Name']}}</router-link>
+      </li>
     </ul>
     <p>{{category}}</p>
     <p>{{channels}}</p>
@@ -16,5 +16,15 @@ import 'bootstrap/dist/css/bootstrap.css'
 export default {
   props: ['category', 'channels'],
   name: 'list-view',
+  methods: {
+    link(category) {
+      return {
+        name: 'list',
+        params: {
+          category: category['Name'],
+        },
+      }
+    },
+  }
 }
 </script>
