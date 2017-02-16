4be08a05b57af86df05f965847113c1df1a0035d
Hui Yiqun <huiyiqun@gmail.com>
2017-02-16 20:59:46 +0800

    load channels async

--- src/main.js
+++ src/main.js
@@ -4,15 +4,23 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 import router from './route.js'
-import channels from './channels.json'
 
-new Vue({
-  el: '#app',
-  template: '' +
-    '<router-view :channels="channels">' +
-    '</router-view>',
-  data: {
-    channels,
-  },
-  router,
+const channelsUrl = require('file-loader?name=[name].[ext]!./channels.json')
+
+const xhr = new XMLHttpRequest()
+xhr.addEventListener('load', function() {
+  const channels = JSON.parse(xhr.response)
+  new Vue({
+    el: '#app',
+    template: '' +
+      '<router-view :channels="channels">' +
+      '</router-view>',
+    data: {
+      channels,
+    },
+    router,
+  })
 })
+xhr.open('GET', channelsUrl)
+xhr.send()
+
