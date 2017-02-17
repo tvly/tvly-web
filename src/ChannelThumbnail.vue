13ed852ef1d8e6f587c0f68cfef123fccf70577b
Hui Yiqun <huiyiqun@gmail.com>
2017-02-17 22:23:09 +0800

    add a fab to play

--- src/ChannelThumbnail.vue
+++ src/ChannelThumbnail.vue
@@ -3,16 +3,18 @@
     <div class="card">
       <div class="card-image">
         <img :src="snapshot">
-        <span class="card-title">{{channel['Name']}}</span>
+        <router-link :to="channelLink" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">play_arrow</i></router-link>
       </div>
-      <div class="card-action">
-        <router-link :to="channelLink">Play</router-link>
+      <div class="card-content">
+        <p>{{channel['Name']}}</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import 'material-design-icons/iconfont/material-icons.css'
+
 export default {
   name: 'channel-thumbnail',
   props: ['channel'],
