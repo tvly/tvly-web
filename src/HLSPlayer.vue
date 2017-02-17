dc6a0e16ad522dd8559ca931d4f55e9eee542da3
Hui Yiqun <huiyiqun@gmail.com>
2017-02-17 12:01:25 +0800

    flowplayer with hls.js

---
+++ src/HLSPlayer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="player"></div>
+  </div>
+</template>
+
+<script>
+import 'flowplayer/dist/skin/skin.css'
+import flowplayer from 'flowplayer'
+import engine from 'flowplayer-hlsjs'
+engine(flowplayer)
+
+export default {
+  props: ['channel', 'channels'],
+  name: 'hls-player',
+  mounted() {
+    console.log(this.$el.childNodes[0])
+    flowplayer(this.$el.childNodes[0], {
+      clip: {
+        sources: [{
+          type: 'application/x-mpegurl',
+          src: '//stream.flowplayer.org/bauhaus.m3u8',
+        }],
+      },
+      debug: true,
+    })
+  },
+}
+</script>
--- src/route.js
+++ src/route.js
@@ -1,6 +1,7 @@
 import VueRouter from 'vue-router'
 
 import ListView from './ListView.vue'
+import HLSPlayer from './HLSPlayer.vue'
 
 import channels from './channels.json'
 
@@ -21,6 +22,11 @@ const routes = [{
   path: '/list/:category',
   props: true,
   component: ListView,
+}, {
+  name: 'play',
+  path: '/play/:channel',
+  props: true,
+  component: HLSPlayer,
 }]
 
 export default new VueRouter({
