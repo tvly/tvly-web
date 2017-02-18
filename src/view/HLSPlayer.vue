150447aed5b44f1eeb035385efae3c04375f0f5d
Hui Yiqun <huiyiqun@gmail.com>
2017-02-18 19:49:43 +0800

    use keyIdentifier instead

--- src/view/HLSPlayer.vue
+++ src/view/HLSPlayer.vue
@@ -44,7 +44,7 @@ export default {
       } else if (event.key === 'ArrowDown' || event.key === 'j') {
         this.switchCategory(1)
       } else {
-        console.log(`Unkown key event: ${event.key}(${event.code})`, event)
+        console.log(`Unkown key event: ${event.key}(${event.keyIdentifier})`)
       }
     },
     switchChannel(offset) {
