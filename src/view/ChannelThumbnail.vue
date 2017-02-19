a7fabb66c06a94b529bb6da36ad19ae21d1e4933
Hui Yiqun <huiyiqun@gmail.com>
2017-02-19 16:10:19 +0800

    remove wave

--- src/view/ChannelThumbnail.vue
+++ src/view/ChannelThumbnail.vue
@@ -3,7 +3,7 @@
     <div class="card">
       <div class="card-image">
         <img :src="snapshot" @click="$router.push(channelLink)">
-        <router-link :to="channelLink" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">play_arrow</i></router-link>
+        <router-link :to="channelLink" class="btn-floating halfway-fab red"><i class="material-icons">play_arrow</i></router-link>
       </div>
       <div class="card-content">
         <p>{{channel['Name']}}</p>
