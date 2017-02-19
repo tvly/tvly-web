40be1a10029b04c7d6e020b73d42f659dfd4fcd0
Hui Yiqun <huiyiqun@gmail.com>
2017-02-19 20:44:22 +0800

    add babel-polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/main.js',
+  entry: ['babel-polyfill', './src/main.js'],
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'build.js',
