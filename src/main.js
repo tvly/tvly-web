b773528023bf9c88498f3cf4b2a88dbdc02517ae
Hui Yiqun <huiyiqun@gmail.com>
2017-03-07 14:22:30 +0800

    add animation

--- src/main.js
+++ src/main.js
@@ -4,15 +4,29 @@ import VueRouter from 'vue-router';
 import {router} from './route/route.js';
 import channels from './data/channels.json';
 
+import './animation.css';
+
 Vue.use(VueRouter);
 
 new Vue({
   el: '#app',
   template: '' +
-    '<router-view :channels="channels">' +
-    '</router-view>',
+    '<transition :name="transition">' +
+      '<router-view :channels="channels">' +
+      '</router-view>' +
+    '</transition>',
   data: {
     channels,
+    transition: '',
+  },
+  watch: {
+    '$route'(to, from) {
+      if (from.name === 'list' && to.name === 'play') { // from list to play
+        this.transition = 'slide-left';
+      } else {
+        this.transition = 'slide-right';
+      }
+    },
   },
   router,
 });
