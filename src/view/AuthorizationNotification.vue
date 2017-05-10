7ad177171923427005446e5b4f2981aaa77db581
Hui Yiqun <huiyiqun@gmail.com>
2017-05-10 14:55:11 +0800

    add flow-text

--- src/view/AuthorizationNotification.vue
+++ src/view/AuthorizationNotification.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="" id="auth-notification" class="modal">
-    <div class="modal-content">
+    <div class="modal-content flow-text">
       <h4 v-if="auth">未授权访问</h4>
       <h4 v-else>网络故障</h4>
       <p v-if="auth">有一些资源不能访问，可能是因为你当前在校外或错误地设置了代理。我们即将为您跳转到学校的统一认证平台进行登录，登录成功之后可以返回当前网页继续观看。如果登录成功之后依然出现以上该错误，可以尝试配置浏览器开启三方cookie。</p>
