9e29acc02944b424878e1e0630316697177f027e
Hui Yiqun <huiyiqun@gmail.com>
2017-05-14 18:34:00 +0800

    rewrite footer with vuetify

--- src/view/IPTVFooter.vue
+++ src/view/IPTVFooter.vue
@@ -1,15 +1,15 @@
 <template>
-  <footer class="page-footer teal lighten-2">
-    <div class="container">
-      <div class="row">
-        <div class="col l6 s12">
+  <v-footer class="footer">
+    <v-container>
+      <v-row>
+        <v-col md6 sm12>
           <h5 class="white-text">鸣谢</h5>
           <p v-if="srcCredit.length" class="grey-text text-lighten-5">感谢{{srcCredit}}提供视频源</p>
           <p class="grey-text text-lighten-5">
             感谢 <a href="http://www.flaticon.com/authors/madebyoliver" class="grey-text text-lighten-5">Madebyoliver</a> 的 Logo
           </p>
-        </div>
-        <div class="col l4 offset-l2 s12">
+        </v-col>
+        <v-col md4 offset-md2 sm12>
           <h5 class="white-text">联系我们</h5>
           <ul class="zmdi-hc-ul">
             <li><a class="grey-text text-lighten-3" target="_blank" rel="noopener" href="https://github.com/huiyiqun/iptv"><i class="zmdi zmdi-github zmdi-hc-li"></i>Github</a></li>
@@ -17,15 +17,13 @@
             <li v-if="wechat && wechat.length"><a class="grey-text text-lighten-3"><i class="fa fa-li fa-wechat" aria-hidden="true"></i>{{wechat}}</a></li>
             <li v-if="telegram && telegram.length"><a class="grey-text text-lighten-3" target="_blank" rel="noopener" :href="'https://t.me/joinchat/' + telegram"><i class="fa fa-li fa-telegram" aria-hidden="true"></i>Telegram</a></li>
           </ul>
-        </div>
-      </div>
-    </div>
-    <div class="footer-copyright">
-      <div class="container">
+        </v-col>
+      </v-row>
+      <v-row>
         Powered by {{sponsor}} © 2017 Ver.{{version}}@{{built.toLocaleString()}}
-      </div>
-    </div>
-  </footer>
+      </v-row>
+    </v-container>
+  </v-footer>
 </template>
 
 <script>
@@ -47,14 +45,8 @@ export default {
 };
 </script>
 
-<style>
-div#body {
-  display: flex;
-  min-height: 100vh;
-  flex-direction: column;
-}
-
-main {
-  flex: 1 0 auto;
+<style scoped>
+.footer {
+  height: auto;
 }
 </style>
