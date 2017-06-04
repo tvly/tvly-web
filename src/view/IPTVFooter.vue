bb9f89a368bc29b3029cd8515972d4f59bc69596
Hui Yiqun <huiyiqun@gmail.com>
2017-06-04 09:59:33 +0800

    IPTVFooter: rewrite css as scss

--- src/view/IPTVFooter.vue
+++ src/view/IPTVFooter.vue
@@ -47,14 +47,15 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 div#body {
   display: flex;
   min-height: 100vh;
   flex-direction: column;
+
+  main {
+    flex: 1 0 auto;
+  }
 }
 
-main {
-  flex: 1 0 auto;
-}
 </style>
